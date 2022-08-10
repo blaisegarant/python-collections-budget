e69c4b8e2737c7b71b468965f818f198d33fec15
Blaise Garant <blaise@garantcoutu.com>
2022-08-10 16:59:25 -0400

    Module 1 task 1

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -9,6 +9,7 @@ expenses = Expense.Expenses()
 expenses.read_expenses("data/spending_data.csv")
 
 spending_categories = []
+
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
