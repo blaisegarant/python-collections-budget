98987eab0e5cfd34d5e6e9b928d1d4088d9271e3
Blaise Garant <blaise@garantcoutu.com>
2022-08-14 17:23:53 -0400

    correct MutableMapping

--- tests/utils.py
+++ tests/utils.py
@@ -19,7 +19,7 @@ def convert_ast(node, return_type='string', include_type=False, sep=':'):
         items = []
         for k, v in d.items():
             new_key = parent_key + sep + k if parent_key else k
-            if isinstance(v, collections.MutableMapping):
+            if isinstance(v, collections.abc.MutableMapping):
                 items.extend(_flatten_dict(v, new_key, sep=sep).items())
             else:
                 items.append((new_key, v))
